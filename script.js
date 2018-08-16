4efa9061b8bfb4b1041a70f38fd62a351b9398f7
CDuncan142 <CDuncan142@West-MEC.org>
2018-08-15 18:27:07 -0700

    Set up
    signed edited documents
    created script.js
    linked script.js
    added global variable for form validation
    added event listener.

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+      Project 01_06_01
+
+      Author: Conner Duncan
+      Date:   08.15.18
+
+      Filename: index.html
+*/
+
+"use strict";
+
+// global variables
+var formValidity = true;
+
+// event listeners
+window.createEventListeners("load", alert("hi"));
